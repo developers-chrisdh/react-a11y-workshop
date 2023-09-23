3019fea2f72f6a526c6012f176a2aff520c4395f
Maxim <mxmvveen@gmail.com>
2023-09-23 15:33:00 +0200

    accessible table

--- src/pages/webshop/components/ProductPage/ProductPage.js
+++ src/pages/webshop/components/ProductPage/ProductPage.js
@@ -68,44 +68,43 @@ const ProductPage = () => {
 
             <div className="product-details">
               <table>
-                <tbody>
-                  <tr>
-                    <td>Was:</td>
-                    <td>
-                      <span>
-                        Sojawas die zorgt voor een rookarme, consistente
-                        verbranding
-                      </span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Geur:</td>
-                    <td>
-                      <span>
-                        Hoogwaardige ingrediënten met natuurlijke essentiële
-                        oliën
-                      </span>
-                    </td>
-                  </tr>
-                </tbody>
+                <tr>
+                  <th>Was:</th>
+                  <td>
+                    <span>
+                      Sojawas die zorgt voor een rookarme, consistente
+                      verbranding
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Geur:</th>
+                  <td>
+                    <span>
+                      Hoogwaardige ingrediënten met natuurlijke essentiële oliën
+                    </span>
+                  </td>
+                </tr>
               </table>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Brandtijd:</td>
-                    <td>
-                      <span>70-75 uur</span>
-                    </td>
-                    <td>Afmeting:</td>
-                    <td>
-                      <span>10cm x 5cm</span>
-                    </td>
-                    <td>Gewicht:</td>
-                    <td>
-                      <span>400g</span>
-                    </td>
-                  </tr>
-                </tbody>
+              <table style={{ display: 'flex' }}>
+                <tr>
+                  <th>Brandtijd:</th>
+                  <td>
+                    <span>70-75 uur</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Afmeting:</th>
+                  <td>
+                    <span>10cm x 5cm</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th>Gewicht:</th>
+                  <td>
+                    <span>400g</span>
+                  </td>
+                </tr>
               </table>
             </div>
           </div>
