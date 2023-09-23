8130605bc4010185a64d46fce72f8bb010b7b9c0
Maxim <mxmvveen@gmail.com>
2023-09-23 16:12:26 +0200

    .

--- src/pages/webshop/components/ProductPage/PurchaseOption.js
+++ src/pages/webshop/components/ProductPage/PurchaseOption.js
@@ -1,27 +1,27 @@
-import { Link } from 'react-router-dom';
-import './PurchaseOption.scss';
+// import { Link } from 'react-router-dom';
+// import './PurchaseOption.scss';
 
-const PurchaseOption = (props) => {
-  const { value, isChecked, onPurchageTypeChange, title, description, link } =
-    props;
+// const PurchaseOption = (props) => {
+//   const { value, isChecked, onPurchageTypeChange, title, description, link } =
+//     props;
 
-  return (
-    <div className={`purchase-type ${isChecked ? 'selected' : ''}`}>
-      <label htmlFor={value}>
-        <input
-          type="radio"
-          value={value}
-          checked={isChecked}
-          onChange={onPurchageTypeChange}
-          id={value}
-        />
-        <span className="radiobutton" />
-        {title}
-        {description && <p>{description}</p>}
-      </label>
-      {link && <Link to={link.url}>{link.label}</Link>}
-    </div>
-  );
-};
+//   return (
+//     <div className={`purchase-type ${isChecked ? 'selected' : ''}`}>
+//       <label htmlFor={value}>
+//         <input
+//           type="radio"
+//           value={value}
+//           checked={isChecked}
+//           onChange={onPurchageTypeChange}
+//           id={value}
+//         />
+//         <span className="radiobutton" />
+//         {title}
+//         {description && <p>{description}</p>}
+//       </label>
+//       {link && <Link to={link.url}>{link.label}</Link>}
+//     </div>
+//   );
+// };
 
-export default PurchaseOption;
+// export default PurchaseOption;
