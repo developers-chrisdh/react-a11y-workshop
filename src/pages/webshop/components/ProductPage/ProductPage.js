4104501aedcc64e4bd6f9c7ef23f51c24b0560b1
cdheij <Chris.Heij@logius.nl>
2023-10-26 14:18:10 +0200

    merge with main. Fix conflicts

--- src/pages/webshop/components/ProductPage/ProductPage.js
+++ src/pages/webshop/components/ProductPage/ProductPage.js
@@ -54,7 +54,9 @@ const ProductPage = () => {
               korting wordt toegepast in het afrekenproces."
                   link={{
                     url: '#',
-                    label: 'Lees meer.',
+                    // In deze situatie gaf alleen "lees meer" niet genoeg context om de actie begrijpelijk te maken
+                    // Door simpelweg de tekst aan te vullen met het doel van de tekst voldoen we aan de WCAG richtlijn
+                    label: 'Lees meer over het abonnement.',
                   }}
                 />
                 <button onClick={onButtonClick}>
